f16747a0885f71e4a8c7a2dc2907491e584b19e6
Devion Rosa <devionrosa@gmail.com>
2025-06-30 19:14:08 +0000

    Added test file

---
+++ testing.py
@@ -0,0 +1,11 @@
+import unittest
+from main import Main
+
+class TestMain(unittest.TestCase):
+    # Three different cases
+
+    #Case for youtube status code
+
+    #Case for genai status code
+
+    #Case for user input
